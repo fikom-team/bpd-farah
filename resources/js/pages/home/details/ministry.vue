9127537969f01c27bb0bfe38fcd3383ccc1b5114
luck_nut <hasyrawi@gmail.com>
2020-08-30 05:09:29 +0800

    create details ministry

---
+++ resources/js/pages/home/details/ministry.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="detail">
+    <div class="header-label">
+      <span>Detail Sasaran Strategis</span>
+    </div>
+    <div class="flex mt-2">
+      <Card :contents="ministry" />
+    </div>
+    <div class="detail-card">
+      <Card-Detail :kementrian="ministry.kementrian" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from "../../../components/molecules/Card";
+import CardDetail from "../../../components/molecules/DetailCard";
+import Axios from "axios";
+export default {
+  components: {
+    Card,
+    CardDetail,
+  },
+
+  data: () => ({
+    ministry: {},
+  }),
+
+  beforeMount() {
+    this.getDetailMinistry();
+  },
+  methods: {
+    getDetailMinistry() {
+      const { id } = this.$route.params;
+      Axios.get("/api/sp/" + id).then((res) => {
+        this.ministry = res.data.data;
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.detail-card {
+  @apply mt-3;
+}
+</style>
