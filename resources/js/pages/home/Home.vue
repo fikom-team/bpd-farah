86986fbc3879d95673bb55cc5c0fed19d85a7b02
luck_nut <hasyrawi@gmail.com>
2020-08-30 05:11:06 +0800

    styling header component

--- resources/js/pages/home/Home.vue
+++ resources/js/pages/home/Home.vue
@@ -48,17 +48,6 @@ export default {
 
 
 <style scoped>
-.header-label {
-  @apply bg-gradient-to-r;
-  @apply from-gray-500;
-  @apply font-bold;
-  @apply mx-2;
-  @apply w-56;
-  @apply mt-12;
-  @apply p-2;
-  @apply rounded-tl-lg;
-  @apply tracking-wider;
-  @apply text-white;
-}
+
 </style>
 
